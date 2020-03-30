da1a7ccbd4ef9c8d60facc2749f08e2b30977f70
Galia <haixia.cheng@mircosoft.com>
2020-03-30 13:21:25 +0800

    Doc for Setup Simple Cluster Manually

---
+++ WebLogic on AKS/Setup Simple Cluster Manually/README.md
@@ -0,0 +1,46 @@
+The page describe how to set up simple WebLogic cluster on ASK, we will recommand you to run the following commands with Azure Cloud Shell.  
+
+1. Start Azure Cloud Shell
+If you don't know how to start Azure Cloud Shell, please go to [Use Azure Cloud Shell](https://docs.microsoft.com/en-us/azure/aks/kubernetes-walkthrough#use-azure-cloud-shell).   
+
+1. Create AKS cluster  
+AKS is a managed Kubernetes service that lets you quickly deploy and manage clusters. To learn more, please go to [Azure Kubernetes Service (AKS)](https://docs.microsoft.com/en-us/azure/aks/). We will deploy an Azure Kubernetes Service (AKS) cluster using the Azure CLI.  
+``
+# Create aks cluster
+az aks create --resource-group $AKS_PERS_RESOURCE_GROUP \
+--name $AKS_CLUSTER_NAME \
+--vm-set-type VirtualMachineScaleSets \
+--node-count 3 \
+--generate-ssh-keys \
+--kubernetes-version 1.14.8 \
+--nodepool-name nodepool1 \
+--node-vm-size Standard_D4s_v3 \
+--location $AKS_PERS_LOCATION
+``
+After the deployment successes, run the fowllowing command to connect to aks instance.  
+``
+az aks get-credentials --resource-group $AKS_PERS_RESOURCE_GROUP --name $AKS_CLUSTER_NAME
+``
+To verify the connection to your cluster, use the kubectl get command to return a list of the cluster nodes.  
+``
+kubectl get nodes
+``
+Example output:  
+``
+NAME                                STATUS   ROLES   AGE     VERSION
+aks-nodepool1-58449474-vmss000000   Ready    agent   2d20h   v1.14.8
+aks-nodepool1-58449474-vmss000001   Ready    agent   2d20h   v1.14.8
+aks-nodepool1-58449474-vmss000002   Ready    agent   2d20h   v1.14.8
+``
+
+2. Create storage and set up file share  
+
+3. Install WebLogic Operator  
+4. Create WebLogic Domain  
+5. Deploy sample application  
+6. Troubleshooting  
+
+https://docs.microsoft.com/en-us/azure/aks/kubernetes-walkthrough
+https://oracle.github.io/weblogic-kubernetes-operator/userguide/introduction/introduction/
+https://docs.microsoft.com/en-us/azure/aks/azure-files-volume
+
---
+++ WebLogic on AKS/Setup Simple Cluster Manually/setup-simple-cluster.sh
@@ -0,0 +1,89 @@
+# Change these four parameters as needed for your own environment
+AKS_PERS_RESOURCE_GROUP=wls-aks-simple-cluster$RANDOM
+AKS_CLUSTER_NAME=WLSSimpleCluster$RANDOM
+AKS_PERS_STORAGE_ACCOUNT_NAME=wlssimplecluster$RANDOM
+AKS_PERS_LOCATION=eastus
+AKS_PERS_SHARE_NAME=weblogic
+
+echo $AKS_PERS_RESOURCE_GROUP $AKS_CLUSTER_NAME $AKS_PERS_STORAGE_ACCOUNT_NAME
+
+# Create a resource group
+az group create --name $AKS_PERS_RESOURCE_GROUP --location $AKS_PERS_LOCATION
+
+# Create aks cluster
+az aks create --resource-group $AKS_PERS_RESOURCE_GROUP \
+--name $AKS_CLUSTER_NAME \
+--vm-set-type VirtualMachineScaleSets \
+--node-count 3 \
+--generate-ssh-keys \
+--kubernetes-version 1.14.8 \
+--nodepool-name nodepool1 \
+--node-vm-size Standard_D4s_v3 \
+--location $AKS_PERS_LOCATION
+
+# Create a storage account
+az storage account create -n $AKS_PERS_STORAGE_ACCOUNT_NAME -g $AKS_PERS_RESOURCE_GROUP -l $AKS_PERS_LOCATION --sku Standard_LRS
+
+# Export the connection string as an environment variable, this is used when creating the Azure file share
+export AZURE_STORAGE_CONNECTION_STRING=$(az storage account show-connection-string -n $AKS_PERS_STORAGE_ACCOUNT_NAME -g $AKS_PERS_RESOURCE_GROUP -o tsv)
+
+# Create the file share
+az storage share create -n $AKS_PERS_SHARE_NAME --connection-string $AZURE_STORAGE_CONNECTION_STRING
+
+# Get storage account key
+STORAGE_KEY=$(az storage account keys list --resource-group $AKS_PERS_RESOURCE_GROUP --account-name $AKS_PERS_STORAGE_ACCOUNT_NAME --query "[0].value" -o tsv)
+
+# Echo storage account name and key
+echo Storage account name: $AKS_PERS_STORAGE_ACCOUNT_NAME
+echo Storage account key: $STORAGE_KEY
+
+# Connect to AKS cluster
+az aks get-credentials --resource-group $AKS_PERS_RESOURCE_GROUP --name $AKS_CLUSTER_NAME
+
+# Create a Kubernetes secret
+kubectl create secret generic azure-secret \
+--from-literal=azurestorageaccountname=$AKS_PERS_STORAGE_ACCOUNT_NAME \
+--from-literal=azurestorageaccountkey=$STORAGE_KEY
+
+# Mount the file share as a volume
+kubectl apply -f pv.yaml
+kubectl get pv azurefile -o yaml
+kubectl apply -f pvc.yaml
+kubectl get pvc azurefile -o yaml
+
+# Grant the Helm service account the cluster-admin role
+kubectl apply -f grantHelmRole.yaml
+
+# Check pod stuats
+kubectl -n kube-system get pods -w
+
+# Helm
+helm version
+helm init
+helm repo add weblogic-operator https://oracle.github.io/weblogic-kubernetes-operator/charts
+helm repo update
+
+# For Helm 3.x
+helm install weblogic-operator weblogic-operator/weblogic-operator
+
+# Create WebLogic Domain Credentials
+git clone https://github.com/oracle/weblogic-kubernetes-operator
+cd weblogic-kubernetes-operator/kubernetes/samples/scripts/create-weblogic-domain-credentials
+./create-weblogic-credentials.sh -u weblogic -p welcome1 -d domain1
+
+# Create Docker Credentials
+kubectl create secret docker-registry regcred \
+--docker-server=docker.io \
+--docker-username=username \
+--docker-password=password \
+--docker-email=test@example.com
+
+# Create Weblogic Domain
+cd ../create-weblogic-domain/domain-home-on-pv
+./create-domain.sh -i domain1.yaml -o ~/azure/output -e -v
+
+kubectl  apply -f admin-lb.yaml
+kubectl  get svc
+
+kubectl  apply -f cluster-lb.yaml
+kubectl  get svc
