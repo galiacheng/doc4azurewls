529107b82ff6d927dd32151908b69e2aea39ce9f
Galia Cheng <haixia.cheng@microsoft.com>
2022-04-19 09:55:45 +0800

    Delete WebLogicOnAKS/SetupSimpleClusterManually/weblogic-kubernetes-operator directory

--- WebLogicOnAKS/SetupSimpleClusterManually/weblogic-kubernetes-operator/operator/src/main/java/oracle/kubernetes/operator/watcher/package-info.java
+++
@@ -1,5 +0,0 @@
-// Copyright (c) 2017, 2020, Oracle Corporation and/or its affiliates.
-// Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
-
-/** Watch support, provides a stream of events when Kubernetes objects are changed. */
-package oracle.kubernetes.operator.watcher;
